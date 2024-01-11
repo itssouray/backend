3d62571f6480e7f8cc9b751bc98d047184ed0f28
sourav <itssouravgupta@gmail.com>
2024-01-12 01:04:31 +0530

    jwt updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import express from "express";
+import dotenv from "dotenv";
 import connect from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path:"./env"
+});
 
 
-connect();
+
+connect()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is running at port : ",process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("MONOG DB connection failed !!! ",err);
+})
